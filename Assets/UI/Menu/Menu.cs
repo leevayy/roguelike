d029cdc2004bedd065a7aaf5b1adf1d3a8688d8a
leevayy <maoaoa7@gmail.com>
2025-08-10 13:59:11 +0300

    chore: idk

--- Assets/UI/Menu/Menu.cs
+++ Assets/UI/Menu/Menu.cs
@@ -8,15 +8,15 @@ using utility;
 public class Menu : MonoBehaviour
 {
     private UIDocument _document;
-    
+
     private TextElement _playButton;
-    
+
     //Add logic that interacts with the UI controls in the `OnEnable` methods
     private void OnEnable()
     {
         // The UXML is already instantiated by the UIDocument component
         _document = GetComponent<UIDocument>();
-        
+
         _playButton = _document.rootVisualElement.Q<Button>("play");
 
         _playButton.RegisterCallback<ClickEvent>((evt =>
